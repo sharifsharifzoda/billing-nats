7925f08b84b608747c54c4d24e8a0c2566c63df2
sharifsharifzoda <sharifzodastudy@gmail.com>
2023-07-17 14:28:01 +0500

    2023-07-17

--- billing-nats-bot/handler/bot.go
+++ billing-nats-bot/handler/bot.go
@@ -4,37 +4,110 @@ import (
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/nats-io/nats.go"
-	"log"
 )
+
+const (
+	commandStart = "start"
+)
+
+var ChatId []int64
 
 func (h *Handler) SendMessageToBot(msg *nats.Msg) {
 	fmt.Println(string(msg.Data))
 
-	update := tgbotapi.NewUpdate(0)
-	update.Timeout = 60
+	//update := tgbotapi.NewUpdate(0)
+	//update.Timeout = 60
 
-	fmt.Println(string(msg.Data))
-	updates, err := h.Bot.GetUpdatesChan(update)
-	if err != nil {
-		h.Logger.Println("failed to get updates. Error is:", err.Error())
-		return
+	//fmt.Println(string(msg.Data))
+	//updates, err := h.Bot.GetUpdatesChan(update)
+	//if err != nil {
+	//	h.Logger.Println("failed to get updates. Error is:", err.Error())
+	//	return
+	//}
+
+	//for update := range updates {
+	//	if strings.ToLower(update.Message.Text) == "/news" {
+	//		trId := fmt.Sprintf("Transaction #%v completed successfully", string(msg.Data))
+	//		fmt.Println("Received:", update.Message.Text, trId, "->", update.Message.From)
+	//
+	//		message := tgbotapi.NewMessage(update.Message.Chat.ID, trId)
+	//		_, err := h.Bot.Send(message)
+	//		if err != nil {
+	//			log.Println("failed to send the message to bot. Error is:", err.Error())
+	//			continue
+	//		}
+	//
+	//	}
+	//
+	//	if strings.ToLower(update.Message.Text) == "/start" {
+	//		ChatId = append(ChatId, update.Message.Chat.ID)
+	//		fmt.Println(update.Message.Chat.ID)
+	//	}
+	//
+	//	for _, id := range ChatId {
+	//		log.Println("sending...")
+	//		message := tgbotapi.NewMessage(id, string(msg.Data))
+	//		_, err := h.Bot.Send(message)
+	//		if err != nil {
+	//			log.Println("failed to send the message to bot. Error is:", err.Error())
+	//			continue
+	//		}
+	//	}
+	//
+	//}
+
+	//message := tgbotapi.NewMessage(1238370443, string(msg.Data))
+	//_, err := h.Bot.Send(message)
+	//if err != nil {
+	//	log.Println("failed to send the message to bot. Error is:", err.Error())
+	//}
+}
+
+func (h *Handler) handleCommand(message *tgbotapi.Message) error {
+	switch message.Command() {
+	case commandStart:
+		return h.handleStartCommand(message)
+	default:
+		return h.handleUnknownCommand(message)
 	}
+}
 
-	for update := range updates {
-		//if update.Message.Text == "start"
-		if update.Message != nil {
-			trId := fmt.Sprintf("Transaction #%v completed successfully", string(msg.Data))
-			fmt.Println("Received:", update.Message.Text, trId, "->", update.Message.From)
+func (h *Handler) handleStartCommand(message *tgbotapi.Message) error {
+	msg := tgbotapi.NewMessage(message.Chat.ID, "Already authorized")
+	_, err := h.Bot.Send(msg)
+	return err
+}
 
-			message := tgbotapi.NewMessage(update.Message.Chat.ID, trId)
-			_, err := h.Bot.Send(message)
-			if err != nil {
-				log.Println("failed to send the message to bot. Error is:", err.Error())
-				continue
-			}
+func (h *Handler) handleMessage(message *tgbotapi.Message, ch chan *nats.Msg) error {
+	//for sub.IsValid() {
+	//	m, err := sub.NextMsg(10 * time.Second)
+	//	if err != nil {
+	//		log.Println("Error is: ", err.Error())
+	//		return err
+	//	}
+	//
+	//	msg := tgbotapi.NewMessage(message.Chat.ID, string(m.Data))
+	//	_, err = h.Bot.Send(msg)
+	//	if err != nil {
+	//		return err
+	//	}
+	//}
+	//
+	//return nil
 
+	for msg := range ch {
+		newMessage := tgbotapi.NewMessage(message.Chat.ID, string(msg.Data))
+		_, err := h.Bot.Send(newMessage)
+		if err != nil {
+			return err
 		}
-
 	}
 
+	return nil
+}
+
+func (h *Handler) handleUnknownCommand(message *tgbotapi.Message) error {
+	msg := tgbotapi.NewMessage(message.Chat.ID, "Unknown command")
+	_, err := h.Bot.Send(msg)
+	return err
 }
--- billing-nats-bot/handler/handler.go
+++ billing-nats-bot/handler/handler.go
@@ -21,15 +21,39 @@ func NewHandler(nats *nats.Conn, bot *tgbotapi.BotAPI, log *log.Logger) *Handler
 }
 
 func (h *Handler) Init() {
-	sub, err := h.Nats.Subscribe("bot.*", h.SendMessageToBot)
-	if err != nil {
-		log.Fatalf("can't subscribe to the subject. Error is: %s", err.Error())
-	}
-	sub, err = h.Nats.Subscribe("bot.*", h.Test)
+	msgChan := make(chan *nats.Msg, 5)
+
+	sub, err := h.Nats.ChanSubscribe("bot.*", msgChan)
 	if err != nil {
 		log.Fatalf("can't subscribe to the subject. Error is: %s", err.Error())
 	}
 	defer sub.Unsubscribe()
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := h.Bot.GetUpdatesChan(u)
+	if err != nil {
+		h.Logger.Fatalln("failed to get updates. Error is:", err.Error())
+	}
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if update.Message.IsCommand() {
+			if err := h.handleCommand(update.Message); err != nil {
+				h.handleError(update.Message.Chat.ID, err)
+			}
+
+			continue
+		}
+
+		if err := h.handleMessage(update.Message, msgChan); err != nil {
+			h.handleError(update.Message.Chat.ID, err)
+		}
+	}
 
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT)
@@ -38,6 +62,7 @@ func (h *Handler) Init() {
 	log.Println("Shutting down!")
 }
 
-func (h *Handler) Test(msg *nats.Msg) {
-	fmt.Println("printing from test ----", string(msg.Data))
+func (h *Handler) handleError(id int64, err error) {
+	msg := tgbotapi.NewMessage(id, err.Error())
+	h.Bot.Send(msg)
 }
